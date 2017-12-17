4c9ba9a141699676b106ecad4fa5f4f5f11873ff
finch185277 <31809727+finch185277@users.noreply.github.com>
2017-12-17 15:10:00 +0800

    Update login.js
    1.
    .get()  .post() 
    change to $.ajax({
    	    method : "GET",....
    2.
    add [contentType: "multipart/form-data", ]

--- login.js
+++ login.js
@@ -6,6 +6,7 @@ function login() {
     //Sync account and password
     var account, password, number;
     var accountSet = true;
+    var png ;
     
 
     chrome.storage.sync.get(["Account", "Password"], function(User) {
@@ -28,38 +29,57 @@ function login() {
         else {
             console.log("can't find password");
         }
-
 	
-        var x = $("#captcha");
-        console.log(x);
+        // var x = $("#captcha");
+        // console.log(x);
+	// console.log(x.width());
+	
+	//var captcha = $('<img src="' + "/captcha/pic.php" + '" alt="">');
+	// console.log(x);
 
-        number = $.post("https://nasa.cs.nctu.edu.tw/sap/2017/hw2/captcha-solver/api/",
-                        x,
-                        function(data,status){
-    			    //console.log("Data: " + data + "\nStatus: " + status);
-			    console.log(status);
-			    if ($("seccode") != null)
-				document.getElementById("seccode").value = status;
-			    else {
-				console.log("can't find captcha");
+	// var cookie_text = $.cookie('cookie_name', 'cookie_value');
+	// console.log(cookie_value);
+
+	$.ajax({
+	    method : "GET",
+	    url : "/captcha/pic.php",
+	    ContentType: "image/png",
+	    success : function(data, status){            
+		console.log(data);
+		$.ajax({
+		    url : "https://nasa.cs.nctu.edu.tw/sap/2017/hw2/captcha-solver/api/",
+                    data : data,
+		    cache: false,
+		    contentType: "multipart/form-data",
+		    processData: false,
+		    type: 'POST',
+                    success : function(data){
+    			console.log("Data: " + data + "\nStatus: " + status);
+			//console.log(status);
+			if ($("seccode") != null)
+			    document.getElementById("seccode").value = data;
+			else {
+			    console.log("can't find captcha");
+			}
+			var loginBtn =$("input[name='Submit2']");
+			//if toggle is on : Login
+			var toggle_state;
+			chrome.storage.sync.get("toggle", function(result) {
+			    toggle_state = result.toggle;
+			    //console.log("toggle_state = " + toggle_state);
+			    if (toggle_state === undefined) {
+				console.log(toggle_state);
 			    }
-			    var loginBtn =$("input[name='Submit2']");
-			    //if toggle is on : Login
-			    var toggle_state;
-			    chrome.storage.sync.get("toggle", function(result) {
-				toggle_state = result.toggle;
-				//console.log("toggle_state = " + toggle_state);
-				if (toggle_state === undefined) {
-				    console.log(toggle_state);
-				}
-				if (toggle_state == true && accountSet) {
-				    loginBtn.click();
-				    top.location.replace('login.php');
-				}
-			    });
-
-    			}
-    		       );
-        console.log("end test");
-    });
+			    if (toggle_state == true && accountSet) {
+				loginBtn.click();
+				top.location.replace('login.php');
+			    }
+			});
+		    }
+		});
+		console.log("end test");
+	    }
+	});   
+    }
+			   );
 }
